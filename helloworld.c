4d8f9a68e5454d5fae3b6c28f96de0dc1aed82e4
NoalsyyII <noahsimpson@hotmail.com>
2024-03-05 12:35:17 +1000

    Second Commit

--- helloworld.c
+++ helloworld.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
+#define MAX 32
 
 int main(){
-    printf("Hello World!");
+
+    char name[MAX];
+
+    printf("Enter Name:");
+
+    scanf("%32s", name);
+
+    printf("Hello %s!", name);
+
+    
 
     return 0;
+
 }
