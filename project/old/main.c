b2632b9b2fdac1c97eaec6ac65fbecb910b87721
NoalsyII <noahsimpson@hotmail.com>
2024-04-30 23:01:28 +1000

    Structures Implemented

--- project/old/main.c
+++ project/old/main.c
@@ -6,49 +6,30 @@
 #define MAX_STRING 50
 #define MAX_ARRLEN 100
 
-typedef struct book {
-    int ISBN;
-    int isborrowed;
-    char title[MAX_STRING];
-    char author[MAX_STRING];
-} Book;
+int bookcount = 0, usercount = 0, bookids[MAX_ARRLEN], userids[MAX_ARRLEN];
 
-typedef struct user {
-    int ID;
-    char name[MAX_STRING];
-    Book borrowedbooks[MAX_ARRLEN];
-} User;
+char titles[MAX_ARRLEN][MAX_STRING], usernames[MAX_ARRLEN][MAX_STRING];
 
-int bookcount = 0, usercount = 0;
-Book books[MAX_ARRLEN];
-User users[MAX_ARRLEN];
-
-//Menu
+//Menu Functions
 void mainMenu(int*);
 void manageBooks();
 void manageUsers();
 
-//Generic Handlers
-int deleteFromRecords(int, int*, char);
-int findMe(int, int, char);
+//Shared Functions
+int deleteFromRecords(int, int*, char[][MAX_STRING], int*);
+int findMe(int, int*, int);
 
-//Book Management
+//Book management deliverables
 void addBook();
 void viewAllBooks();
 void updateBook();
 void deleteBook();
 
-//User Management
+// //User management deliverables
 void registerUser();
 void viewAllUsers();
 void updateUser();
 void deleteUser();
-
-//File I/O
-int loadBooks();
-int loadUsers();
-int saveBook(Book);
-int saveUser(User);
 
 int main(){
     printf("Welcome to the library management system!\n");
@@ -65,7 +46,8 @@ int main(){
 void mainMenu(int* mainloopflag){
 //Handles main menu functionality, takes a pointer to the main menu loop flag as input.
     int mainchoice;
-    void (*mainoptions[2])() = {&manageUsers, &manageBooks};
+
+    void (*mainselection)();
 
     while(*mainloopflag){
         //system("cls");
@@ -73,22 +55,28 @@ void mainMenu(int* mainloopflag){
         printf("MAIN MENU\n1: User Management\n2: Book Management\n3: Exit\n>>");
         scanf("%d", &mainchoice);
 
-        if(mainchoice < 1 || mainchoice > 2){
-            *mainloopflag = 0;
-            return;
+        switch(mainchoice){
+            case 1:
+                mainselection = &manageUsers;
+                break;
+            case 2:
+                mainselection = &manageBooks;
+                break;
+            default:
+                *mainloopflag = 0;
+                return;
         }
-
-        (*mainoptions[mainchoice - 1])();
-
+        (*mainselection)();
     }
 }
 
 void manageUsers(){
 //Handles user management menu functionality
     //system("cls");
+    
     int userloopflag = 1, userchoice;
 
-    void (*useroptions[4])() = {&viewAllUsers, &registerUser, &updateUser, &deleteUser};
+    void (*userselection)();
 
     while(userloopflag){
 
@@ -97,21 +85,35 @@ void manageUsers(){
         printf("USER MANAGEMENT MENU\nThere are %d current users\n1: View all users\n2: Register new user\n3: Update user\n4: Delete user\n5: Back to main menu\n>>", usercount);
         scanf("%d", &userchoice);
 
-        if(userchoice < 1 || userchoice > 4){
-            userloopflag = 0;
-            return;
+        switch(userchoice){
+            case 1:
+                userselection = &viewAllUsers;
+                break;
+            case 2:
+                userselection = &registerUser;
+                break;
+            case 3:
+                userselection = &updateUser;
+                break;
+            case 4:
+                userselection = &deleteUser;
+                break;
+            default:
+                userloopflag = 0;
+                return;
         }
 
-        (*useroptions[userchoice - 1])();
+        (*userselection)();
     }
 }
 
 void manageBooks(){
 //Handles book management menu functionality
     //system("cls");
+    
     int bookloopflag = 1, bookchoice;
 
-    void (*bookoptions[4])() = {&viewAllBooks, &addBook, &updateBook, &deleteBook};
+    void (*bookselection)();
 
     while(bookloopflag){
 
@@ -120,12 +122,24 @@ void manageBooks(){
         printf("BOOK MANAGEMENT MENU\nThere are %d current titles\n1: View all titles\n2: Register new title\n3: Update book information\n4: Delete title\n5: Back to main menu\n>>", bookcount);
         scanf("%d", &bookchoice);
 
-        if(bookchoice < 1 || bookchoice > 4){
-            bookloopflag = 0;
-            return;
+        switch(bookchoice){
+            case 1:
+                bookselection = &viewAllBooks;
+                break;
+            case 2:
+                bookselection = &addBook;
+                break;
+            case 3:
+                bookselection = &updateBook;
+                break;
+            case 4:
+                bookselection = &deleteBook;
+                break;
+            default:
+                bookloopflag = 0;
+                return;
         }
-
-        (*bookoptions[bookchoice - 1])();
+        (*bookselection)();
     }
 }
 
@@ -140,7 +154,7 @@ void viewAllUsers(){
     }
 
     for(int i = 0; i < usercount; i++){
-        printf("ID: %d | Name: %s\n", users[i].ID, users[i].name);
+        printf("Name: %s | ID: %d\n", usernames[i], userids[i]);
     }
 
 }
@@ -157,8 +171,8 @@ void registerUser(){
     
     tempname[strcspn(tempname, "\n")] = 0; //cuts off newline char
 
-    users[usercount].ID = usercount;
-    strcpy(users[usercount].name, tempname);
+    userids[usercount] = usercount;
+    strcpy(usernames[usercount], tempname);
 
     printf("%s added sucessfully!\n", tempname);
 
@@ -166,9 +180,38 @@ void registerUser(){
 }
 
 void updateUser(){
-    //placeholder
-    printf("Updating User");
-    return;
+//Handles menu for updating user, passing to appropriate functions where necessary.
+    int searchID, index, choice;
+    printf("Enter user ID to change details >>");
+    scanf("%d", &searchID);
+    
+    if((index = findMe(searchID, userids, usercount)) == -1){
+        printf("User with ID %d not found, try again", searchID);
+        return;
+    }
+    
+    printf("User found\nprintf(Name: %s | ID: %d\n", usernames[index], userids[index]);
+    printf("1: Update ID\n2: Update Name\n3: Cancel\n>>", bookcount);
+    scanf("%d", &choice);
+
+    switch(choice){
+            case 1:
+                userselection = &viewAllUsers;
+                break;
+            case 2:
+                userselection = &registerUser;
+                break;
+            case 3:
+                userselection = &updateUser;
+                break;
+            case 4:
+                userselection = &deleteUser;
+                break;
+            default:
+                return;
+        }
+
+
 }
 
 void deleteUser(){
@@ -179,8 +222,8 @@ void deleteUser(){
     printf("Enter User ID >>");
     scanf(" %d", &tempID);
 
-    if(deleteFromRecords(tempID, &usercount, 'u')){
-        printf("User ID: %d deleted successfully!\n", tempID);
+    if(deleteFromRecords(tempID, userids, usernames, &usercount)){
+        printf("ISBN %d deleted successfully!\n", tempID);
     }
 
 }
@@ -194,19 +237,19 @@ void viewAllBooks(){
     }
 
     for(int i = 0; i < bookcount; i++){
-        printf("ISBN: %d | Title: %s | Author: %s | Borrowed: %s\n", books[i].ISBN, books[i].title, books[i].author, (books[i].isborrowed ? "Yes" : "No") );
+        printf("Title: %s | ISBN: %d\n", titles[i], bookids[i]);
     }
 }
 
 void addBook(){
-//Menu function for adding book, checks for duplicate additions before updating array.
+//Menu function for adding book, checks for duplicate additions before updating arrays.
     printf("Add New Title:\n");
     int tempISBN;
-    char temptitle[MAX_STRING], tempauthor[MAX_STRING];    
+    char temptitle[MAX_STRING];
 
     printf("Enter Book ISBN >>");
     scanf(" %d", &tempISBN);
-    if(findMe(tempISBN, bookcount, 'b') != -1){
+    if(findMe(tempISBN, bookids, bookcount) != -1){
         printf("Book with ISBN %d already exists!, try again\n", tempISBN);
         return;
     }
@@ -217,15 +260,9 @@ void addBook(){
     fgets(temptitle, MAX_STRING - 1, stdin);
     
     temptitle[strcspn(temptitle, "\n")] = 0; //cuts off newline char
-    
-    printf("Enter Book Author >>");
-    fgets(tempauthor, MAX_STRING - 1, stdin);
-    
-    tempauthor[strcspn(tempauthor, "\n")] = 0; //cuts off newline char
 
-    books[bookcount].ISBN = tempISBN;
-    strcpy(books[bookcount].title, temptitle);
-    strcpy(books[bookcount].author, tempauthor);
+    bookids[bookcount] = tempISBN;
+    strcpy(titles[bookcount], temptitle);
 
     printf("%s added sucessfully!\n", temptitle);
 
@@ -244,28 +281,23 @@ void deleteBook(){
     printf("Enter Book ISBN >>");
     scanf(" %d", &tempISBN);
 
-    if(deleteFromRecords(tempISBN, &bookcount, 'b')){
+    if(deleteFromRecords(tempISBN, bookids, titles, &bookcount)){
         printf("ISBN %d deleted successfully!\n", tempISBN);
     }
 
 }
 
-int deleteFromRecords(int _IDtodelete, int *counter, char mode){
+int deleteFromRecords(int _IDtodelete, int* idlist, char namelist[][MAX_STRING], int* counter){
 //Generic handler function to delete from records, takes record arrays and an ID for deletion
-    int index = findMe(_IDtodelete, *counter, mode);
+    int index = findMe(_IDtodelete, idlist, *counter);
 
     if(index == -1){
         return 0;
     }
 
-    if(mode == 'b'){
-        for(int i = index; i < *counter - 1; i++){
-        books[i] = books[i + 1];
-    }
-    } else{
-        for(int i = index; i < *counter - 1; i++){
-        users[i] = users[i + 1];
-    }
+    for(int i = index; i < *counter - 1; i++){
+        idlist[i] = idlist[i + 1];
+        strcpy(namelist[i], namelist[i + 1]);
     }
 
     *counter -= 1;
@@ -273,30 +305,14 @@ int deleteFromRecords(int _IDtodelete, int *counter, char mode){
     return 1;
 }
 
-int findMe(int ID, int _counter, char mode){
+int findMe(int ID, int* idlist, int _counter){
 //Returns index of a given ID for any ID list provided
     int index = -1;
-
-    if(mode == 'b'){
-        for(int i = 0; i < _counter; i++){
-            if(books[i].ISBN == ID){
-                index = i;
+    for(int i = 0; i < _counter; i++){
+        if(idlist[i] == ID){
+            index = i;
         }
     }
-    } else{
-        for(int i = 0; i < _counter; i++){
-            if(users[i].ID == ID){
-                index = i;
-        }
-        }
-    }
-
     return index;
 
 }
-
-
-
-
-
-
