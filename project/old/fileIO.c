65355fbdf6fadaf06daeb2e7cfc64a0ce002bfab
NoalsyyII <noahsimpson@hotmail.com>
2024-05-04 20:55:05 +1000

    Improved File IO

--- project/old/fileIO.c
+++ project/old/fileIO.c
@@ -32,5 +32,65 @@ int saveUser(User);
 
 
 int main(){
+    // Book testbook, testbook1;
+    // testbook.ISBN = 1234;
+    // testbook.isborrowed = 0;
+    // strcpy(testbook.title, "The Adventures of Soah Nimpson");
+    // strcpy(testbook.author, "N.T Simpson");
+    // books[0] = testbook;
+
+    // testbook1.ISBN = 5678;
+    // testbook1.isborrowed = 0;
+    // strcpy(testbook1.title, "Shartmeister");
+    // strcpy(testbook1.author, "Noah TS");
+    // books[1] = testbook1;
+
+    // for(int i = 0; i < 2; i++){
+    //     if(saveBook(books[i])){
+    //         printf("Book %d saved successfully\n", books[i].ISBN);
+    //     }
+    // }
+
+    loadBooks();
+
+    printf("Count: %d\n", bookcount);
+    for(int i = 0; i < bookcount; i++){
+    printf("%d | %s | %s | %s\n", books[i].ISBN, books[i].title, books[i].author, books[i].isborrowed? "Yes":"No");
+    }
+
+}
+
+int saveBook(Book tosave){
+    FILE *catalogue = NULL;
+
+    if((catalogue = fopen("catalogue.txt", "a")) == NULL){
+        return 0;
+    }
+
+    fprintf(catalogue, "%d\t%s\t%s\t%d\n",tosave.ISBN, tosave.title, tosave.author, tosave.isborrowed);
+
+    fclose(catalogue);
+
+    return 1;
+
+}
+
+int loadBooks(){
+    FILE *catalogue = NULL;
+    int tempISBN, tempisborrowed;
+    char temptitle[MAX_STRING], tempauthor[MAX_STRING];
+    Book tempbook;
+
+    if((catalogue = fopen("catalogue.txt", "r")) == NULL){
+        return 0;
+    }
+    while(!feof(catalogue)){
+        fscanf(catalogue, "%d\t%s\t%s\t%d", &tempbook.ISBN, tempbook.title, tempbook.author, &tempbook.isborrowed);
+        books[bookcount] = tempbook;
+        bookcount++;
+    }
+
+    return 1;
+
 
 }
